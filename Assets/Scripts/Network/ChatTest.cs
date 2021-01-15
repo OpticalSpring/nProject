bbf83b3fb7d00d03ae0ca4a5b4e889b71fea686b
OpticalSpring <cosmos5477@gmail.com>
2021-01-15 11:09:48 +0900

    채팅 닉네임 나오게 추가

--- Assets/Scripts/Network/ChatTest.cs
+++ Assets/Scripts/Network/ChatTest.cs
@@ -10,10 +10,12 @@ public class ChatTest : MonoBehaviourPunCallbacks
     public Text mainText;
     public InputField chatField;
     bool chatEnabled;
+    public Color nameColor;
+    public string namePlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -34,34 +36,46 @@ public class ChatTest : MonoBehaviourPunCallbacks
                 chatEnabled = true;
             }
         }
-            
+
     }
 
-    
+
+    public static string ColorToStr(Color color) 
+    { 
+        string r = ((int)(color.r * 255)).ToString("X2"); 
+        string g = ((int)(color.g * 255)).ToString("X2"); 
+        string b = ((int)(color.b * 255)).ToString("X2"); 
+        string a = ((int)(color.a * 255)).ToString("X2"); 
+        string result = string.Format("{0}{1}{2}{3}", r, g, b, a); 
+        return result; 
+    }
 
 
     void Chat()
     {
-        
-            string c = chatField.text;
-            if(c.Length > 20)
-            {
+        string c = chatField.text;
+        if (c.Length == 0)
+        {
+            return;
+        }
+        if (c.Length > 20)
+        {
             c.Substring(0, 20);
-            }
-           // SendChatMessage(c);
-            PhotonView photonView = PhotonView.Get(this);
-            photonView.RPC("SendChatMessage", RpcTarget.All, c);
-            chatField.text = "";
-        
+        }
+        string result = string.Format("<color=#{0}>[{1}]</color> ", ColorToStr(nameColor), namePlayer);
+        result += c;
+        PhotonView photonView = PhotonView.Get(this);
+        photonView.RPC("SendChatMessage", RpcTarget.All, result);
+        chatField.text = "";
     }
 
     [PunRPC]
     private void SendChatMessage(string inputLine)
-	{
-		if (string.IsNullOrEmpty(inputLine))
-		{
-			return;
-		}
-        mainText.text += "\n"+ inputLine;
+    {
+        if (string.IsNullOrEmpty(inputLine))
+        {
+            return;
+        }
+        mainText.text += "\n" + inputLine;
     }
 }
